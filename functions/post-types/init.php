42c03f22391e59ee5f541920b08bd20f6ca1ef93
Gennie Harris <gh374@cornell.edu>
2024-01-22 17:27:33 -0500

    Remove duplicate get_checked_post_types function

--- functions/post-types/init.php
+++ functions/post-types/init.php
@@ -6,20 +6,6 @@ $cptui_post_types = cptui_get_post_type_slugs();
 // Get checked post types
 $checked_post_types = get_checked_post_types();
 
-// Get chosen post_types based on theme options checked boxes
-//if ( ! function_exists ( 'get_checked_post_types' ) ) {
-	function get_checked_post_types() {
-
-		$post_type_options = get_field('post_type_options', 'options');
-
-		if($post_type_options) {
-			$checked_post_types = $post_type_options['post_types'];
-			return $checked_post_types;
-		}
-
-	}
-//}
-
 // Initialize built-in post types
 if($checked_post_types) :
 	foreach($checked_post_types as $checked_post_type) {
