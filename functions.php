c806a29c15ab4bba6bfe513c51f119b6c0baf40b
dc774 <dc774@cornell.edu>
2025-04-11 15:23:35 -0400

    Update functions.php. Using locate_template makes it simpler to override parent functions in child theme. It looks in the child theme first for the file. Then you just have to follow the file structure in your child theme.

--- functions.php
+++ functions.php
@@ -8,38 +8,37 @@
 *******************************************************/
 
 // Required files
-require_once 'functions/saml_auth.php';
-require_once 'functions/post-types/general.php';
-require_once 'functions/theme/setup.php';
-require_once 'functions/theme/minify.php';
-require_once 'functions/customizer/customize-register.php';
-require_once 'functions/theme/layout.php';
-require_once 'functions/theme/body-classes.php';
-require_once 'functions/theme/pagination.php';
-require_once 'functions/theme/widgets.php';
-require_once 'functions/theme/metadata.php';
-require_once 'functions/theme/dates.php';
-require_once 'functions/theme/images.php';
-require_once 'functions/theme/header-img.php';
-require_once 'functions/theme/header-slider.php';
-require_once 'functions/theme/options.php';
-require_once 'functions/theme/gallery.php';
-require_once 'functions/theme/styles-and-scripts.php';
-require_once 'functions/theme/templating-functions.php';
-require_once 'functions/navigation/menus.php';
-require_once 'functions/navigation/breadcrumbs.php';
-require_once 'functions/navigation/menu-classes.php';
-require_once 'functions/navigation/section-nav/section-nav.php';
-//require_once 'functions/theme/custom-fields/featured.php';
-require_once 'functions/theme/custom-fields/featured-image.php';
-require_once 'functions/theme/custom-fields/header-slider.php';
-require_once 'functions/theme/custom-fields/image_id.php';
-require_once 'functions/theme/custom-fields/page_links_to.php';
-require_once 'functions/theme/custom-fields/remove-header-image.php';
-require_once 'functions/theme/custom-fields/acf-search.php';
-require_once 'functions/tinymce/editor.php';
-require_once 'blocks/block_functions.php';
-require_once 'blocks/custom_block_fields.php';
+require_once locate_template('/functions/post-types/general.php');
+require_once locate_template('/functions/theme/setup.php');
+require_once locate_template('/functions/theme/minify.php');
+require_once locate_template('/functions/customizer/customize-register.php');
+require_once locate_template('/functions/theme/layout.php');
+require_once locate_template('/functions/theme/body-classes.php');
+require_once locate_template('/functions/theme/pagination.php');
+require_once locate_template('/functions/theme/widgets.php');
+require_once locate_template('/functions/theme/metadata.php');
+require_once locate_template('/functions/theme/dates.php');
+require_once locate_template('/functions/theme/images.php');
+require_once locate_template('/functions/theme/header-img.php');
+require_once locate_template('/functions/theme/header-slider.php');
+require_once locate_template('/functions/theme/options.php');
+require_once locate_template('/functions/theme/gallery.php');
+require_once locate_template('/functions/theme/styles-and-scripts.php');
+require_once locate_template('/functions/theme/templating-functions.php');
+require_once locate_template('/functions/navigation/menus.php');
+require_once locate_template('/functions/navigation/breadcrumbs.php');
+require_once locate_template('/functions/navigation/menu-classes.php');
+require_once locate_template('/functions/navigation/section-nav/section-nav.php');
+//require_once locate_template('/functions/theme/custom-fields/featured.php');
+require_once locate_template('/functions/theme/custom-fields/featured-image.php');
+require_once locate_template('/functions/theme/custom-fields/header-slider.php');
+require_once locate_template('/functions/theme/custom-fields/image_id.php');
+require_once locate_template('/functions/theme/custom-fields/page_links_to.php');
+require_once locate_template('/functions/theme/custom-fields/remove-header-image.php');
+require_once locate_template('/functions/theme/custom-fields/acf-search.php');
+require_once locate_template('/functions/tinymce/editor.php');
+require_once locate_template('/blocks/block_functions.php');
+require_once locate_template('/blocks/custom_block_fields.php');
 
 // Initialize post types if ACF plugin is present and activated
 if ( class_exists('Acf') ) {
